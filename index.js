cd7047dbc76f5f924b8f0c2b1590ccba19766e8d
Samantha Crespo <samkcrespo@gmail.com>
2022-04-05 21:19:25 -0700

    commit tree 200ed04db677fc5a024732543e346241aa99b94a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const returnFirstTwoDrivers = (drivers) => ['Antonia', 'Nuru'];
+
+const returnLastTwoDrivers = (drivers) => ['Amari', 'Mo'];
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+    const fareMultiplier = (fare) => {
+        return multiplier * fare;
+    };
+    return fareMultiplier;
+};
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare);
+};
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+};
+
+function selectDifferentDrivers (array, returnFirstTwoDrivers) {
+return returnFirstTwoDrivers(array); 
+
+};
 // Code your solution in this file!
